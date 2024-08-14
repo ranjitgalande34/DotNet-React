792d55b5cf5db98c4871d87e4743700401de3172
Ranjit Galande <105547231+ranjitgalande34@users.noreply.github.com>
2024-08-14 14:46:01 +0530

    Adding Assignments

---
+++ Assignments/MVC/Assignment_1/Controllers/SecondController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC_Demo2.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Assignments/MVC/Assignment_1/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace MVC_Demo2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            // Custom Middleware
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("/end"))
+                {
+                    await context.Response.WriteAsync("Terminating middleware when URL contains /end");
+                    return;
+                }
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("hello"))
+                {
+                    await context.Response.WriteAsync("Hello ");
+                }
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("hello"))
+                {
+                    await context.Response.WriteAsync("Hello1 ");
+                }
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("hello"))
+                {
+                    await context.Response.WriteAsync("Hello2 ");
+                }
+                await next();
+            });
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
